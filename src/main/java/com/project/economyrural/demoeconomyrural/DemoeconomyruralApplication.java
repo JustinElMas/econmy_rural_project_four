a1f2789c03ec0c7ab097e8d2a22ac79d9e3c10e8
JustinElMas <justin7b.jjr2014@gmail.com>
2023-08-18 00:33:22 -0500

    Inicializando proyecto de Spring Boot + Java / Grupo 4 APP Economia Rural

---
+++ src/main/java/com/project/economyrural/demoeconomyrural/DemoeconomyruralApplication.java
@@ -0,0 +1,13 @@
+package com.project.economyrural.demoeconomyrural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoeconomyruralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoeconomyruralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/economyrural/demoeconomyrural/DemoeconomyruralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.economyrural.demoeconomyrural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoeconomyruralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
